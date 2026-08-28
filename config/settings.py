93654472a16ea2c93c02f352be73582ac02ae109
ronnie <dumpmail.exe@gmail.com>
2026-08-28 16:55:24 +0530

    postgres orm added

--- config/settings.py
+++ config/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get("POSTGRES_DB", "djangoninja"),
+        'USER': os.environ.get("POSTGRES_USER", "djangoninja"),
+        'PASSWORD': os.environ.get("POSTGRES_PASSWORD", "djangoninja"),
+        'HOST': os.environ.get("POSTGRES_HOST", "127.0.0.1"),
+        'PORT': os.environ.get("POSTGRES_PORT", "5432"),
     }
 }
 
